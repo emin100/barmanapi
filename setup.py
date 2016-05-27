dab76dce39f3c752bb73cd798b0636d6b1cb51e4
mehmet emin karakas <mkarakas@turksat.com.tr>
2016-05-27 14:14:58 +0300

    Fix README

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Framework :: Flask',
-        'Natural Language :: English',
+        'Natural Language :: English'
     ],
 )
